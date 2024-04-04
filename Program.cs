83eebec8323c323ed21d40dfbda4757251210e81
gincalicdan <rogecalicdan011524@gmail.com>
2024-04-04 13:36:54 -0600

    2nd commit

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿/*===============================================================
+Author: Leona Rose Ilao || Rogine Calicdan
+Title: Connect 4 Final Project
+Subject: Introduction to Object-Oriented Programming
+// test
+*/
+
+using System;
+
+namespace ConnectFour
+{
+
+    // Abstract class that represents a player
+
+    abstract class Player
+    {
+
+    }
+
+    // Class representing a human player, inherits from Player
+
+    class HumanPlayer : Player
+    {
+    }
+
+    // Class representing a computer player, inherits from Player
+
+    class ComputerPlayer : Player
+    {
+
+    }
+
+    // Program class
+
+    class Program
+    {
+    }
+
+    // Prints the current game board
+
+  
+
+    // Checks the validity of the move
+
+
+    // Drops the item in the specified column
+
+
+    // Checks the winning combinations
+
+
+    // Check columns
+
+
+    // Check diagonals
+
+
+
+    // Checks if all pieces drop which results to draw
+
+    // Method to handle the game logic
+
+    // Display the outcome
+
+
+    // Main start of the program
+
+  
+}
